b65590d386e136523d72be7eb87dea7d5f4311e8
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 18:45:05 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def bernoulli(p) :
+  # Your code to generate a bernoulli random variable goes here.
+
+def geometric(p): 
+  # Your code to generate a geometric random variable goes here
+  
+print( geometric(0.5), geometric(0.5), geometric(0.5), geometric(0.5) )
