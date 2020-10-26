b7b5fb21ec324dd82aa0a1316c3b6036a24bb8c9
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 18:48:30 +0000

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as st
+import unittest
+import main
+
+class UnitTests(unittest.TestCase) :
+    def test_geometric(self) : 
+       for i in range(1,9) : 
+           p, mean = i*0.1, 0
+           for j in range(100) : 
+               myvar = geometric(p) 
+               self.assertTrue( myvar>0, "the geometric random variable should always be greater than zero" )
+               mean = mean + myvar
+           mean = mean / 100
+           var = ( 1 - p) / (p*p)
+           bar = np.sqrt(var/100)*st.norm.ppf( (0.99 + 1) / 2 )
+           self.assertTrue( np.fabs( mean - 1/p )<bar, "your random variable appears to be sampling from the wrong distribution" )
