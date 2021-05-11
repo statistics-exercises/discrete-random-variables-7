d2140e29e77daa47195f0233b018c0458d50fcd3
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 15:01:55 +0100

    Added answers

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import numpy as np
 
 def bernoulli(p) :
   # Your code to generate a bernoulli random variable goes here.
+  if np.random.uniform(0,1)<p : return 1
+  return 0
 
 def geometric(p): 
   # Your code to generate a geometric random variable goes here
-  
+  nt = 1
+  while bernoulli(p)==0 : nt = nt + 1
+  return nt  
+
 print( geometric(0.5), geometric(0.5), geometric(0.5), geometric(0.5) )
