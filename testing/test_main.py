0a55617f12130152c94e26655872728354b69771
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 15:02:26 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -16,7 +16,7 @@ class UnitTests(unittest.TestCase) :
         inputs, variables = [], []
         for i in range(1,9) :
             p = i*0.1
-            inputs.append((n,i*0.1,))
+            inputs.append((p,))
             myvar = randomvar( 1/p, variance=(1-p)/(p*p), vmin=1, isinteger=True )
             variables.append( myvar )
-        assert( check_func('geometric',inputs, variables, calls=['bernoulli'] ) )
+        assert( check_func('geometric', inputs, variables, calls=['bernoulli'] ) )
